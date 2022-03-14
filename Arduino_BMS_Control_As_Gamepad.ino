b52d61fae9b6ad73f32ff54035873eb1c62b6c75
sr8e <sr@mell0w-5phere.net>
2022-03-14 13:51:43 +0900

    add sketch which works as a Gamepad

---
+++ Arduino_BMS_Control_As_Gamepad.ino
@@ -0,0 +1,118 @@
+#include "HID-Project.h"
+#define INT16_MAX 32767
+#define INT16_MIN -32768
+
+
+//common
+unsigned long elapsed = 0;
+
+//scratch
+const unsigned long ACTIVE_TIME = 150000; //us
+const int THRESHOLD = 750;
+const int THRES_LOW = 850;
+const int THRES_HIGH = 650;
+long scrTimer = 0;
+bool toRight;
+int d1f, d2f;
+int list[8][4] = {
+  {1, 1, 0, 1},
+  {1, 0, 1, 1},
+  {0, 0, 1, 0},
+  {0, 1, 0, 0},
+  {1, 1, 1, 0},
+  {1, 0, 0, 0},
+  {0, 1, 1, 1},
+  {0, 0, 0, 1}
+};
+
+//keys
+const int INTERVAL = 10;
+unsigned long ptime[9];
+
+void setup() {
+  //common
+  Gamepad.begin();
+  for (int i = 2; i < 11; i++) //set pinmode
+    pinMode(i, INPUT_PULLUP);
+  elapsed = micros();
+
+  //scratch
+  toRight = false;
+  d1f = analogRead(A1) < THRESHOLD ? 0 : 1;
+  d2f = analogRead(A2) < THRESHOLD ? 0 : 1;
+
+  //keys
+  for (int i = 0; i < 9; i++)
+    ptime[i] = 0;
+
+}
+
+void loop() {
+  //common
+  unsigned long now = micros();
+  if (now < elapsed) { //overflow!
+    elapsed = now;
+    return;
+  }
+  long tdif = now - elapsed;
+  elapsed = now;
+
+  //scratch
+  int d1l, d2l;
+  int thres1 = d1f == 0 ? THRES_LOW : THRES_HIGH;
+  int thres2 = d2f == 0 ? THRES_LOW : THRES_HIGH;
+  d1l = analogRead(A1) < thres1 ? 0 : 1;
+  d2l = analogRead(A2) < thres2 ? 0 : 1;
+
+  bool triggered = false;
+  int state[4] = {d1f, d1l, d2f, d2l};
+  for (int i = 0; i < 8; i++) {
+    if (memcmp(list[i], state, sizeof(int) * 4) == 0) { //compare
+      if (i < 4) {
+        scrTimer = ACTIVE_TIME;
+        toRight = true;
+        triggered = true;
+        break;
+      } else {
+        scrTimer = ACTIVE_TIME;
+        toRight = false;
+        triggered = true;
+        break;
+      }
+    }
+  }
+  if (!triggered) {
+    scrTimer -= tdif;
+  }
+  if (scrTimer > 0) {
+    if (toRight) {
+      //Right Turn
+      Gamepad.xAxis(INT16_MAX);
+    } else {
+      //Left Turn
+      Gamepad.xAxis(INT16_MIN);
+    }
+  } else {
+    //No Input
+    scrTimer = 0;
+    Gamepad.xAxis(0);
+  }
+  d1f = d1l;
+  d2f = d2l;
+
+  //keys
+  for (int i = 0; i < 9; i++) {
+    ptime[i]++;
+    if (!digitalRead(i + 2)) {
+      Gamepad.press(i + 1);
+      ptime[i] = 0;
+    } else {
+      if (ptime[i] < INTERVAL)return; //burst!
+      Gamepad.release(i + 1);
+    }
+
+  }
+
+  Gamepad.write();
+
+}
